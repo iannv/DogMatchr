d1eda29600ca206114bf5523d97da361e9c1da11
Ian Vázquez <cam.iva@hotmail.com>
2025-12-10 23:22:52 -0300

    Cards inicio

--- frontend/dogapp/src/app/app.routes.ts
+++ frontend/dogapp/src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
-import path from 'path';
 import { Inicio } from './pages/inicio/inicio';
+import { Razas } from './pages/razas/razas';
 
 export const routes: Routes = [
-    {path: '', component: Inicio},
+  { path: '', component: Inicio, runGuardsAndResolvers: 'always' },
+  { path: 'razas', component: Razas },
 ];
