9ff94b9d14277d4c8fa9f46cd8db1eecb0e628c4
Ian Vázquez <cam.iva@hotmail.com>
2025-12-11 23:43:59 -0300

    Card dog

---
+++ frontend/dog/src/app/components/detalle-dog/detalle-dog.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detalle-dog',
+  imports: [],
+  templateUrl: './detalle-dog.html',
+  styleUrl: './detalle-dog.css',
+})
+export class DetalleDog {
+
+}
