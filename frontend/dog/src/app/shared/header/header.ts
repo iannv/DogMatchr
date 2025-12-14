efa94ad0c39c4d8117dc2e7ceaca436bc4e2b589
Ian Vázquez <cam.iva@hotmail.com>
2025-12-13 23:15:55 -0300

    Header

--- frontend/dog/src/app/shared/header/header.ts
+++ frontend/dog/src/app/shared/header/header.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { BadgeModule } from 'primeng/badge';
+import { AvatarModule } from 'primeng/avatar';
+import { InputTextModule } from 'primeng/inputtext';
+import { CommonModule } from '@angular/common';
+import { Ripple } from 'primeng/ripple';
+import { Menubar } from 'primeng/menubar';
+import { RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [Menubar, BadgeModule, AvatarModule, InputTextModule, Ripple, CommonModule, RouterLinkActive],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
+  items: MenuItem[] | undefined;
 
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'INICIO',
+        icon: 'pi pi-home',
+      },
+      {
+        label: 'RAZAS',
+      },
+      {
+        label: '¡ENCONTRAR A MI COMPAÑERO!',
+      },
+    ];
+  }
 }
